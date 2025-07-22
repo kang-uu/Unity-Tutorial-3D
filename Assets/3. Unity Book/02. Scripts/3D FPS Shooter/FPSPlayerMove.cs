eb2b9a6ce86b470417fd066f07ca1dd740daf84a
JeongDay <cidd1332@gmail.com>
2025-07-22 11:51:39 +0900

    2025.07.22 화요일 - 오전시간 2
    캐릭터 모델링 적용 및 애니메이션 기능 적용

--- Assets/3. Unity Book/02. Scripts/3D FPS Shooter/FPSPlayerMove.cs
+++ Assets/3. Unity Book/02. Scripts/3D FPS Shooter/FPSPlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class FPSPlayerMove : MonoBehaviour
 {
     private CharacterController cc;
+    private Animator anim;
 
     public float moveSpeed = 7f;
 
@@ -24,6 +25,7 @@ public class FPSPlayerMove : MonoBehaviour
     void Start()
     {
         cc = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
@@ -37,6 +39,8 @@ public class FPSPlayerMove : MonoBehaviour
         Vector3 dir = new Vector3(h, 0, v); // 크기와 방향이 있는 벡터
         dir = dir.normalized; // 방향만 있는 벡터
 
+        anim.SetFloat("MoveMotion", dir.magnitude);
+        
         // 카메라의 Transform 기준으로 변환
         dir = Camera.main.transform.TransformDirection(dir);
 
