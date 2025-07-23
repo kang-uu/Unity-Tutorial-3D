f34e6939c0de57f561499bdebb607bf5541dbd6e
JeongDay <cidd1332@gmail.com>
2025-07-23 16:38:11 +0900

    2025.07.23 수요일 - 회고시간 1
    NavMesh Bake

--- Assets/3. Unity Book/02. Scripts/Path Finding/AgentController.cs
+++ Assets/3. Unity Book/02. Scripts/Path Finding/AgentController.cs
@@ -12,22 +12,24 @@ public class AgentController : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        index = Random.Range(0, points.Length);
+        SetRandomPoint();
     }
 
     void Update()
     {
-        agent.SetDestination(points[index].position);
-        
         if (agent.remainingDistance <= 1.5f) // 목적지와의 거리가 1.5 이하일 경우
         {
             Debug.Log("목적지 변경");
-
-            int temp = index;
-            index = Random.Range(0, points.Length);
-            
-            if (temp == index)
-                index = Random.Range(0, points.Length);
+            SetRandomPoint();
         }
+    }
+
+    private void SetRandomPoint()
+    {
+        int temp = index;
+        while (temp == index)
+            index = Random.Range(0, points.Length);
+        
+        agent.SetDestination(points[index].position);
     }
 }
