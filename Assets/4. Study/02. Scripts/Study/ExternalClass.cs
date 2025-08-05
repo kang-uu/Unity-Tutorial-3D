3c497252a07396eade2e24fbf1c48959d99109c1
JeongDay <cidd1332@gmail.com>
2025-08-05 11:52:02 +0900

    2025.08.05 화요일 - 오전시간 1
    Action / UnityAction
    커플링 / 디커플링

---
+++ Assets/4. Study/02. Scripts/Study/ExternalClass.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ExternalClass : MonoBehaviour
+{
+    public StudyDelegate studyDelegate;
+    
+    void Start()
+    {
+        StudyAction2.action += OnLog;
+    }
+
+    private void OnLog()
+    {
+        Debug.Log("msg");
+    }
+}
---
+++ Assets/4. Study/02. Scripts/Study/Linq/StudyLinq.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class StudyLinq : MonoBehaviour
+{
+    // var result =  from 변수 in Collection 
+    //               where 조건
+    //               select 조건을 통과한 대상
+}
